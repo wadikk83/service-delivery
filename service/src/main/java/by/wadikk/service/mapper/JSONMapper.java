81c69ecc6f85e3f62449bcdaad8146a7d44e0484
Wadikk <wadikk83@gmail.com>
2021-07-31 02:02:40 +0300

    add maven profiles

--- service/src/main/java/by/wadikk/service/mapper/JSONMapper.java
+++ service/src/main/java/by/wadikk/service/mapper/JSONMapper.java
@@ -5,17 +5,28 @@ import by.wadikk.service.util.MapperFilename;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.Collection;
 
 @Slf4j
+@Component
+@PropertySource("classpath:application.properties")
 @MapperMake(action = "JSON")
 public class JSONMapper extends Mapper {
+
+    @Value("${path.to}")
+    private String pathToTest;
+
     @Override
     public Boolean parse(Class clazz, Collection list, String filename) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
+
+           // System.out.println(pathToTest);
 
             String pathTo = new MapperFilename().getFilename();
             if (pathTo == null) {
--- service/src/main/java/by/wadikk/service/util/MapperFilename.java
+++ service/src/main/java/by/wadikk/service/util/MapperFilename.java
@@ -1,22 +1,29 @@
 package by.wadikk.service.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 @Slf4j
-@Component
 public class MapperFilename {
+
+    @Value("${path.to}")
+    String ppp;
+
     public String getFilename() {
         try {
             Properties prop = new Properties();
-            FileInputStream fileInputStream = new FileInputStream("resources/config.properties");
+            File file = ResourceUtils.getFile("classpath:application.properties");
+            FileInputStream fileInputStream = new FileInputStream(file);
             prop.load(fileInputStream);
-            String phyPath = prop.getProperty("phyPath");
+            String phyPath = prop.getProperty("path.to");
+
             return phyPath;
         } catch (FileNotFoundException exception) {
             log.error("FileNotFoundException !!!!");
